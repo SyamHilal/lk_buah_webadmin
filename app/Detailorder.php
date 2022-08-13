006cd66f61478d5e4cc95550bd1dc43c88964cd0
SyamHilal <106011305+SyamHilal@users.noreply.github.com>
2022-08-14 03:23:08 +0700

    pri2

--- app/Detailorder.php
+++ app/Detailorder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Detailorder extends Model
 {
     protected $table = 'detail_order';
-    protected $fillable = ['order_id','product_id','qty'];
+    protected $fillable = ['order_id','product_id','qty', 'price'];
     public function product()
     {
         return $this->belongsTo(Product::class, 'product_id', 'id');
